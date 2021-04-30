4ec3850d4252eb1f5fce128c954a07a08c91679e
Kadir Taban <32621058+KadirTaban@users.noreply.github.com>
2021-05-01 01:00:36 +0300

    Mask Onn please !

--- src/Mask-Detection.py
+++ src/Mask-Detection.py
@@ -5,6 +5,8 @@ faceCascade = cv2.CascadeClassifier("haarcascade/haarcascade_mask.xml")
 
 video_capture = cv2.VideoCapture(0)
 
+
+
 while video_capture.isOpened():
     # Capture frame-by-frame
     ret, frame = video_capture.read()
@@ -15,17 +17,18 @@ while video_capture.isOpened():
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(30, 30)
+        minSize=(30, 30),
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+
         break
 
 # When everything is done, release the capture
